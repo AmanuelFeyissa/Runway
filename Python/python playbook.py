16b2da58cd55ca6bb7b6d83869aaf929788dc390
Amanuel <amanuelfeyissa45@gmail.com>
2021-11-02 01:41:22 +0300

    fundamentals of python I

---
+++ Python/python playbook.py
@@ -0,0 +1,47 @@
+# To Comment in Python we use the # Sign and write whatever
+# indentation will cause errors
+
+############################################################
+# Printing to console we use the print() function
+# We can use the double quotation inside the parentesis or
+# single quote interchangebly esepecially if we are quoting
+# something in out code we should use single quoting
+# No Semi Colon Needed at the end
+############################################################
+
+print("Hello World")
+
+############################################################
+# To start a new line we use \n
+# To concatinate or combine strings we use + sign
+############################################################
+
+print("Hello" + "World\n")
+
+############################################################
+# To get input from the user we use the input() function
+# input function has a prompt displayer and after the input
+# it will be replaced by the data that was put in
+############################################################
+
+input("What is your name? ")
+
+############################################################
+# len() function displays the length of the string in number
+############################################################
+
+len("AMAN") #OUTPUT 4
+
+############################################################
+# Variables: To create variables we simply write out a
+# letter, a word which Obey the variable naming rules
+# RULES: A number can never come FIRST
+# RULES: No space bettwen letters or words when making a variable
+# RULES: we should not use keywords or function names
+# RULES: we can use underscore for combining words
+# Make sure your variable names are meaningful and readable
+############################################################
+
+name = input("What is your name? ")
+length = 500
+name = "Aman" # Overridden by the string aman
