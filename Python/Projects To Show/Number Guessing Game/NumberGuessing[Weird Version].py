f23493ccd83ef471751fc7b7430140b602f30128
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2021-12-05 21:24:13 +0300

    added useful comments to the number guessing game

--- Python/Projects To Show/Number Guessing Game/NumberGuessing[Weird Version].py
+++ Python/Projects To Show/Number Guessing Game/NumberGuessing[Weird Version].py
@@ -1,13 +1,18 @@
 import random
 
+#RANDOM_NUM store random numbers between 1 and 100
 RANDOM_NUM = random.randint(1, 100)
 
+# Display First
 print("Welcome to the number Guessing Game")
 print("I have a picked a number between 1 - 100")
 
+# difficulity variable for determining the level
 difficulity = input("Pick a difficulity 'easy' or 'hard': ")
 easy = 10
 hard = 5
+
+# If guess is higher or lower than give hints
 def guess_input(num):
   if num > RANDOM_NUM:
     print("Too high")
@@ -19,6 +24,8 @@ def guess_input(num):
     print(f"You are correct the number was {RANDOM_NUM}")
     return
 
+# if easy the attempts becomes 10 and
+# if hard the attempts becomes 5
 if difficulity == "easy":
   while easy != 0:
     print(f"You have {easy} attempts left")
