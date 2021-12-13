81c562a4137ec995457999b9aad8cbf9d0418edb
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2021-12-13 19:47:55 +0300

    Update on flutter intro

--- Mobile App Development/Flutter Playbook.dart
+++ Mobile App Development/Flutter Playbook.dart
@@ -1,2 +1,3 @@
 // It is a framework that is used for building a cross platform applications on Android Web and IOS systems
 // Built by Google and works with the Dart programming language
+// Used for the frontend of an app, and evrything on flutter is a widget
