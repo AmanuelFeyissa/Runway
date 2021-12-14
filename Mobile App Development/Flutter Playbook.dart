0abd2c4e2e32af201e2fbf1cd7b384e57ba9b223
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2021-12-14 20:44:01 +0300

    Intro firebase and flutter

--- Mobile App Development/Flutter Playbook.dart
+++ Mobile App Development/Flutter Playbook.dart
@@ -1,3 +1,4 @@
 // It is a framework that is used for building a cross platform applications on Android Web and IOS systems
 // Built by Google and works with the Dart programming language
-// Used for the frontend of an app, and evrything on flutter is a widget
+// Used for the frontend of an app, and everything on flutter is a widget
+// Worsk well with all databases and especially with the firebase which is from Google itself
