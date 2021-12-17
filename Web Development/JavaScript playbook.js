dfa72d90b29b3018a8e77d1209bca71ba02b10f2
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2021-12-17 23:22:00 +0300

    JavaScript on frontend

--- Web Development/JavaScript playbook.js
+++ Web Development/JavaScript playbook.js
@@ -1,2 +1,3 @@
 JavaScript is the most popular language as of the current date.. 
 It is a scripting language that has a big community
+JavaScript is popular because of it's wide range of frameworks and libraries like react.js, vue.js and angular for frontend development
