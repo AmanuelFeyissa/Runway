a8fde02690e12138c9220be69a17b5ae676ca0a4
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2021-12-18 20:35:45 +0300

    Backend intro

--- Web Development/JavaScript playbook.js
+++ Web Development/JavaScript playbook.js
@@ -1,3 +1,4 @@
-JavaScript is the most popular language as of the current date.. 
-It is a scripting language that has a big community
-JavaScript is popular because of it's wide range of frameworks and libraries like react.js, vue.js and angular for frontend development
+//JavaScript is the most popular language as of the current date.. 
+//It is a scripting language that has a big community
+//JavaScript is popular because of it's wide range of frameworks and libraries like react.js, vue.js and angular for frontend development
+//And for backend the most popular one is Node.js
