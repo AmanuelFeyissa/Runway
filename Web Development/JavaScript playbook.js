4e4bef49f2305002edbed7aede4102c708fa412f
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2021-12-19 22:29:06 +0300

    Mobile app JavaScript

--- Web Development/JavaScript playbook.js
+++ Web Development/JavaScript playbook.js
@@ -2,3 +2,4 @@
 //It is a scripting language that has a big community
 //JavaScript is popular because of it's wide range of frameworks and libraries like react.js, vue.js and angular for frontend development
 //And for backend the most popular one is Node.js
+// React native is a platform that uses the JavaScript language to build mobile application
