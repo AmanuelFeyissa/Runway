5ec10b467ec2851fba8e4b17f6af69eac83c458b
Amanuel Feyissa <43337227+AmanuelFeyissa@users.noreply.github.com>
2022-01-03 20:01:10 +0300

    Comments

--- Mobile App Development/Dart Playbook.dart
+++ Mobile App Development/Dart Playbook.dart
@@ -1,2 +1,5 @@
 // Used for developing mobile app development just like java and kotlin
 // Its not only for mobile development it's used in alot of other platforms
+// Comments
+// Single line comments
+/* Multiple line comments */
