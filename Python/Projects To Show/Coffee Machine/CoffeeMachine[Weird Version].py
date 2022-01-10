17398651a64370167a7e4d86d7cdf7c6e82692fd
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-01-10 19:27:46 +0300

    another file created for coffee machine

---
+++ Python/Projects To Show/Coffee Machine/CoffeeMachine[Weird Version].py
@@ -0,0 +1,36 @@
+# Dictionary for coffee machine which specifies the menu
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+
+usr_input = input("What would you like? (espresso/latte/cappuccino): ")
+
+if usr_input == "espresso":
+
+
+elif usr_input == "off":
+    print("Thank you, GoodBye")
+
