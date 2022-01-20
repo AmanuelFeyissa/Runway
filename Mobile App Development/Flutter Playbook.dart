ef1c417b95d08c51883d8e6c80a9c3651fc27d92
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-01-20 23:07:43 +0300

    Creating flutter project

--- Mobile App Development/Flutter Playbook.dart
+++ Mobile App Development/Flutter Playbook.dart
@@ -1,4 +1,15 @@
 // It is a framework that is used for building a cross platform applications on Android Web and IOS systems
 // Built by Google and works with the Dart programming language
 // Used for the frontend of an app, and everything on flutter is a widget
-// Worsk well with all databases and especially with the firebase which is from Google itself
+// works well with all databases and especially with the firebase which is from Google itself
+
+// Creating a flutter project
+
+// First way using terminal
+/* 1st type flutter create /* name of project with underscore separated */
+   2nd this will generate loads of file that is used for building flutter apps
+   */
+// Second way using VS Code
+/* 1st go to view then click on command palette
+   2nd type 'Flutter: New Project' then add the name of your project
+       use underscores for replacements of spaces */
