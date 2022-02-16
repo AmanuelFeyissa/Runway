14162ebf79e522f4a76b1998c0023d663d18f40a
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-02-16 11:41:40 +0300

    comments

--- Web Development/PHP playbook.php
+++ Web Development/PHP playbook.php
@@ -1,7 +1,13 @@
 <?php 
 /* To write any code in PHP it must be included in these php open and closed tags <?php ?> */
 
+// COMMENTS 
 
+// Single line comments
+# Single line comments
+/* 
+    Multiple line comments
+*/
 
 
 ?>
