2916fcf3dd5dc9c66ec2088db35365e0720907d4
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-02-20 23:21:26 +0300

    Update PHP playbook.php
    note on variables

--- Web Development/PHP playbook.php
+++ Web Development/PHP playbook.php
@@ -9,5 +9,13 @@
     Multiple line comments
 */
 
+// Variables
+// To declare any variables of any datatype in PHP we only use the $ sign infront of it
+// $nameOfVariable
+
+$hello = "hi"; // String
+$num = 123; // Integer
+$flo = 34.54; // Float 
+$bool = true; // boolean
 
 ?>
