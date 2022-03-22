ab85e051b11eda83993663760703316a4cb0010c
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-03-22 21:05:45 +0300

    Creating Functions
    Named Functions
    Anonymous Functions

--- Mobile App Development/Dart Playbook.dart
+++ Mobile App Development/Dart Playbook.dart
@@ -1,9 +1,25 @@
 // Used for developing mobile app development just like java and kotlin
 // Its not only for mobile development it's used in alot of other platforms
 
-
 // Comments
 // Single line comments
 /* Multiple line comments */
 
 // Start when ready today
+
+// Creating Functions
+// Named Function: is when the function is defined by a name
+// Void means it does not return anything
+// doSometing is the name of the function
+// { } inside our curly braces goes our block of code
+// () is where the parameters go
+void doSomething() {
+  // Code Goes Here
+}
+// To call the function
+doSomething();
+
+// Anonymous Function: is when the function has no name
+() {
+  // Code Goes Here
+}
