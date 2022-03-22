dffda0b4fded0f4e06921c02c1268bb265825973
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-03-22 21:42:47 +0300

    Data Type
    primitive data types
    dynamic
    var

--- Mobile App Development/Dart Playbook.dart
+++ Mobile App Development/Dart Playbook.dart
@@ -12,11 +12,29 @@ void main(){
 
 // Creating Variables
 // we use the var keyword then name of our variable
-var num = 5;  
+var num = 5;
 
 // Printing or displaying a Variable we use the print function
 print(num);
 
+// Data Type
+// Dart is a statically typed language meaning it will determine what kind of data
+// type you want when you assign to it and will give an error when you switch one
+// variable of integer to string and others
+var name = 'Aman'; // String
+var num = 5; // int
+var number = 10.5; // double
+var isNice = true; // bool or boolean
+num = 'Hello'; // Gives an error because you changed the data type
+// To Make a variable Dynamic
+dynamic a; // We can change the data types without any Error
+var a; // When a datatype is not determined it will give the variable a dynamic datatype
+// If we want Dart to be data type sensetive
+// Recommended
+String name = 'Hello';
+int num = 5;
+double number = 10.5;
+bool isNice = true;
 // Creating Functions
 // Named Function: is when the function is defined by a name
 // Void means it does not return anything
