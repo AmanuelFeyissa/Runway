73a666e1827e466b8ef33bd16d5376fb7a138385
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-04-16 19:58:05 +0300

    Update JavaScript playbook.js
    added comment

--- Web Development/JavaScript playbook.js
+++ Web Development/JavaScript playbook.js
@@ -1,5 +1,6 @@
 //JavaScript is the most popular language as of the current date.. 
 //It is a scripting language that has a big community
+// it is case sensetive
 //JavaScript is popular because of it's wide range of frameworks and libraries like react.js, vue.js and angular for frontend development
 //And for backend the most popular one is Node.js
 // React native is a platform that uses the JavaScript language to build mobile application
