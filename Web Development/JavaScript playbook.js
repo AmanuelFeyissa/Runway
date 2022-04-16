362d5e6b2c508a0959324547669895543aa3b158
amanuelfeyissa <amanuelfeyissa45@gmail.com>
2022-04-16 21:12:57 +0300

    Conditionals: Nested if

--- Web Development/JavaScript playbook.js
+++ Web Development/JavaScript playbook.js
@@ -93,3 +93,13 @@ if (/*condition is true*/ a == 0) {
  // Execute this code if condition is true
  alert("its true");
 }
+// Nested if
+var balance = 5000;
+if (balance >= 0) {
+    alert("the balance is positive");
+    if ( balance > 10000) {
+        alert("The balance is large")
+    }
+} else {
+    alert("The balance is not positive");
+}
