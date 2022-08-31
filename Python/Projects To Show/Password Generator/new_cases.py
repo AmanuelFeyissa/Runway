8b5dd1a8a4e87c50ef907e10b09e3c379f9d3780
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-08-31 16:30:42 +0300

    python few functions

---
+++ Python/Projects To Show/Password Generator/new_cases.py
@@ -0,0 +1,17 @@
+message = "Hello Eric, Wpuld you like to learn some Python today?"
+print(message)
+name = "Amanuel feyissa"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print('Albert Einstein once said, "A person who never made as mistake never tried anything new."')
+
+man = "  Hello world   "
+print(man.strip())
+print(man.rstrip())
+print(man.lstrip())
+
+print(5 + 3)
+print(11 - 3)
+print(4 * 2)
+print(16 / 2)
