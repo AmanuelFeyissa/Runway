353deb46a6e407ed279d276743fcf0bf7bc027e0
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-09-20 16:37:40 +0300

    working code

--- Python/Projects To Show/list_sorting/list_sorting.py
+++ Python/Projects To Show/list_sorting/list_sorting.py
@@ -1,14 +1,20 @@
 #fname = input("Enter file name: ")
 file_open = open('C:\\Users\\HP\\Documents\\GitHub\\Runway\\Python\\Projects To Show\\list_sorting\\romeo.txt')
 lst = list()
+new_list = []
 for line in file_open:
     xfile = line.rstrip()
     lst = xfile.split()
-    for i in range(len(lst)):
-        single_list = lst[i]
+    for listed in lst:
+        if listed not in new_list:
+            new_list.append(listed)
+    #for i in range(len(lst)):
+        #single_list = lst[i]
         #print(single_list)
-        if single_list is lst[i]:
-            continue
-        else:
-            lst.append(single_list)
-    print(lst)
+        #new_list.append(single_list)
+        #if single_list is lst[i]:
+        #    continue
+        #else:
+        #    lst.append(single_list)
+        #print(lst)
+print(sorted(new_list))
