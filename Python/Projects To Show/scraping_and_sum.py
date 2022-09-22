9ae3e8cf3069792d2bf77ae22f1bdcb38c6ccfdf
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-09-22 14:56:13 +0300

    scraping and sum

--- Python/Projects To Show/scraping_and_sum.py
+++ Python/Projects To Show/scraping_and_sum.py
@@ -2,7 +2,7 @@ import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
 
 html = urllib.request.urlopen('http://py4e-data.dr-chuck.net/comments_1641306.html').read()
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, 'html.parser')
 
 spans = soup('span')
 sum = 0
